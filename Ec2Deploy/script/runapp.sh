c06f79992b17c5b695e24a857588afa96359f672
ELCOT <ELCOT@Lenovo>
2020-08-29 17:43:02 +0530

    CiCdDemo initial commit

---
+++ Ec2Deploy/script/runapp.sh
@@ -0,0 +1,4 @@
+#!usr/bin/env bash
+echo 'Starting my app'
+cd '/home/ec2-user/jarFromCode'
+java -jar 
---
+++ Ec2Deploy/src/main/java/com/example/Ec2Deploy/Controller.java
@@ -0,0 +1,29 @@
+package com.example.Ec2Deploy;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+	@GetMapping("/hello")
+	public String printHello() {
+		return "hello";
+	}
+//
+//	@GetMapping("/say")
+//	public String sayHello() {
+//		return "hello";
+//	}
+//
+//	@GetMapping("/says")
+//	public String saysHello() {
+//		return "saysHello";
+//	}
+//
+//	@GetMapping("/malamalamalemale")
+//	public String malamalamalemale() {
+//		return "malamalamalemale";
+//	}
+
+}
