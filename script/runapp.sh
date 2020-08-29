fb8eefee409333158266fc45fe7a200fd5d64ab6
ELCOT <ELCOT@Lenovo>
2020-08-30 02:09:22 +0530

    CiCdDemo initial commit

--- script/runapp.sh
+++ script/runapp.sh
@@ -1,3 +1,3 @@
+#!/bin/bash
 echo 'Starting my app'
-cd /opt/codedeploy-agent/deployment-root/2d19e47b-5d26-4326-84e6-db60c3729e14/d-UU33AB9X4/deployment-archive/target
 java -jar Ec2Deploy-0.0.1-SNAPSHOT.jar
