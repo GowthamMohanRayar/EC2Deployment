0e060d7b7e5a4efc5e054340a14698b3c11f2ab7
ELCOT <ELCOT@Lenovo>
2020-08-30 18:56:11 +0530

    CiCdDemo initial commit

--- runapp.bash
+++ runapp.bash
@@ -1,5 +1,4 @@
 #!/bin/bash
 echo 'Starting my app'
 cd /home/ec2-user/jarFromCode
-java -jar Ec2Deploy-0.0.1-SNAPSHOT.jar > /dev/null 2>&1
-exit 0
+java -jar Ec2Deploy-0.0.1-SNAPSHOT.jar &
---
+++ stopapp.bash
@@ -0,0 +1,2 @@
+#!/bin/bash
+kill -9 "$(lsof -t Ec2Deploy-0.0.1-SNAPSHOT.jar)"
